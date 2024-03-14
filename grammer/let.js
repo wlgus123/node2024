79db93fae9a01e2a51a5e79cf95890d0d00e18ec
wlgus123 <mail30019794@gmail.com>
2024-03-14 15:42:40 +0900

    let과 var의 차이

---
+++ grammer/let.js
@@ -0,0 +1,17 @@
+{
+    var v = 1;
+    {
+        var v = 2;
+        console.log(v)  // 2
+    }
+    console.log(v)  // 2(주의)
+}
+
+{
+    let v = 1;
+    {
+        let v = 2;
+        console.log(v)  // 2
+    }
+    console.log(v)  // 1
+}
