9bfd6dc5f6ddf2e214f6665bf00867431a85e29d
wlgus123 <mail30019794@gmail.com>
2024-03-21 15:56:19 +0900

    backtick `

---
+++ grammer/backtick.js
@@ -0,0 +1,7 @@
+const a = 'JavaScript'
+const b = 'JS'
+
+console.log(a + ' ' + b)            // JavaScript JS
+
+// backtick을 사용하면 더 직관적이고 편하다
+console.log(`${a}는 ${b}입니다.`)   // JavaScript는 JS
--- grammer/null_undefined.js
+++ grammer/null_undefined.js
@@ -1,14 +1,14 @@
 let foo
-console.log(foo)        // undefined
+console.log(foo) // undefined
 
 const obj = {}
-console.log(obj.prop)   // undefined
+console.log(obj.prop) // undefined
 
 // console.log(bar)     // 에러발생: bar is not defined
 
 let bar = null
-console.log(bar)        // null
+console.log(bar) // null
 
 // 주의
-console.log(null == undefined)      // true
-console.log(null === undefined)     // false
+console.log(null == undefined) // true
+console.log(null === undefined) // false
