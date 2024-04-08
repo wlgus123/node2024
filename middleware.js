72c519e8fe1cf5a550b80290371c5e6c80021747
wlgus123 <mail30019794@gmail.com>
2024-04-08 11:49:34 +0900

    상태저장 아닌 체이닝

--- middleware.js
+++ middleware.js
@@ -6,8 +6,13 @@ const middle1 = require('./my_modules/middle1')
 // arg로 경로를 지정하지 않으면, 모든 경로에 대해 처리
 app.use(middle1)
 
+app.get('/', (req, res, next) => {
+    console.log('Hello')
+    next()
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    console.log('World')
 })
 
 app.listen(3000)
